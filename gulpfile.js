bad5fec34ba876451178b47d8cb758aef4370f14
bugdlr <htdelosreyes8@gmail.com>
2017-12-03 08:29:09 -0800

    added cssmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+
 
 gulp.task('less', function () {
   return gulp.src('./src/**/*.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
+    .pipe(cssmin().on('error', function(err) {
+      console.log(err);
+    }))
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./dist/'));
 });
 
